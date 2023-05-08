11275d97d260c7800284bd912805ebabb79cf343
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-05-08 22:46:27 +0200

    Chapter 12, exercise 6: Reverse order of programs
    By starting off with creating a window too large for the current
    resolution and then resizing it to create a shape that is too big
    for the window, you get slightly different results. Modified
    the Simple_window class to place the 'Next' button at the origin
    because it would not show up when created outside the visible area

--- chapter_12/exercise/e06/e06.cpp
+++ chapter_12/exercise/e06/e06.cpp
@@ -7,22 +7,42 @@
 
 int main()
 {
-    // 1. Shape gets cropped
+    // Let's reverse the order of the programs...
+
+    // 2. A window greater in size than the current resolution
+    // The title bar disappears (The window is 'maximized')
+    // Also the 'Next' button disappears, supposedly because it is
+    // outside the visible area. So the Simple_window class has
+    // been altered to place the button at the origin instead.
     Simple_window win {
-        { 10, 10 }, 640, 480, "Long cat is looooooong"
+        { 0, 0 },
+        static_cast<int>(x_max() * 1.1),
+        static_cast<int>(y_max() * 1.1),
+        "A window that stretches beyond the screen borders"
     };
 
-    Image img { {10,10}, "img/longcat.jpg"};
+    Graph_lib::Text txt { {50, 50}, "This window is really big" };
+    txt.set_font_size(24);
+    txt.set_color(Color::black);
+    win.attach(txt);
 
-    win.attach(img);
     win.wait_for_button();
 
-    // 2. Window stretches past the extents of the screen,
-    // Moving it by clicking on the title bar resizes it to
-    // fit on the x-axis only, the height is unaffected.
-    const int ww { x_max() + 500 };
-    const int wh { y_max() + 500 };
-
+    // 1. An image that is too high to fit inside the window.
+    // The image gets cropped, but can be viewed in its full after
+    // resizing the window
+    const int ww { 640 };
+    const int wh { 360 };
     win.resize(ww, wh);
+
+    Image img { { 120, 10 }, "img/longcat.jpg"};
+
+    win.label("A shape that stretches beyond the window borders");
+
+    txt.set_label("This cat is really loooong");
+    win.attach(img);
+    win.detach(txt); // Move  text to front
+    win.attach(txt);
+
     win.wait_for_button();
 }
