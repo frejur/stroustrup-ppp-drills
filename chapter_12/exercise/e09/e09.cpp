8a30330ba0b6a63ad59044b1aeba6f0a6063c305
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-05-12 23:33:18 +0200

    Finish Chapter 12, exercise 9

---
+++ chapter_12/exercise/e09/e09.cpp
@@ -0,0 +1,34 @@
+#include "../../lib/Simple_window.h"
+#include "../../lib/Graph.h"
+#include <system_error>
+#include <iostream>
+
+int main()
+{
+    try
+    {
+        const string txt { "A friend" };
+        Point tl{ 100, 100 };
+        Simple_window win{ tl, 600, 400, txt };
+
+        // Image
+        Image img{ Point{ 100, 50 }, "img/a_friend.gif"};
+        win.attach(img);
+
+        // Label
+        Text label { { 100, 365 }, txt };
+        label.set_font(Font::screen_bold);
+        label.set_font_size(18);
+        label.set_color(Color::black);
+        win.attach(label);
+
+        win.wait_for_button();
+        return 0;
+    }
+    catch (exception& e) {
+        return 1;
+    }
+    catch (...) {
+        return 2;
+    }
+}
