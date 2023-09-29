13f6f9656012bf18f144a2c25fa7ddd7b2dfd8bb
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-09-29 04:54:44 +0200

    CH12 E11, add pentagon

--- chapter_12/exercise/e11/e11.cpp
+++ chapter_12/exercise/e11/e11.cpp
@@ -23,7 +23,12 @@ using namespace Graph_lib;
 
 double sec(double ang_rad)
 {
-    return 1 / std::cos(ang_rad);
+    return 1.0 / std::cos(ang_rad);
+}
+
+double cot(double ang_rad)
+{
+    return std::cos(ang_rad) / std::sin(ang_rad);
 }
 
 double deg_to_rad(float ang_in_deg) {
@@ -35,7 +40,7 @@ double deg_to_rad(float ang_in_deg) {
 int main()
 {
     const int ww { 840 };
-    const int wh { 480 };
+    const int wh { 680 };
     Simple_window win{
         {0,0}, ww, wh, "Inscribe Polyons" };
 
@@ -43,7 +48,7 @@ int main()
     const int tri_rad { 200 };
     int tri_side = static_cast<int>(round(std::sqrt(3.0) * tri_rad));
     RegularPolygon tri {
-        {420, 340}, tri_rad, 3, (float)deg_to_rad(90 + 15) };
+        {520, 440}, tri_rad, 3, (float)deg_to_rad(90 + 15) };
     tri.set_color(Color::black);
     tri.set_style(
         Line_style(Line_style::Line_style_type::solid, 1));
@@ -69,9 +74,6 @@ int main()
     sqr.set_style(
         Line_style(Line_style::Line_style_type::solid, 1));
     win.attach(sqr);
-
-
-    win.wait_for_button();
 
     /* Pentagon
      *
@@ -110,5 +112,37 @@ int main()
      *
      * Source: https://www.graphicalcommunication.com/pdfs/drawinglargestsquareinsideapolygon.pdf
      */
+
+
+    int pen_offs_x {
+        (int)round(
+            (sqr_side *
+             sin(deg_to_rad(18) * sin(deg_to_rad(54)))) /
+            sin(deg_to_rad(108))
+        )};
+    int pen_offs_y {
+        (int)round(pen_offs_x / tan(deg_to_rad(54))) };
+    int pen_top_w {
+        (int)round(sqr_side + pen_offs_x * 2) };
+    int pen_side {
+        (int)round(
+            (pen_top_w * 0.5) / sin(deg_to_rad(54))
+        )};
+    int pen_rad {
+        (int)round(
+            (pen_side * 0.5) / cos(deg_to_rad(54))) };
+    RegularPolygon pen {
+        { sqr.point(3).x - pen_offs_x * 1,
+          sqr.point(3).y + pen_offs_y * 1},
+        pen_rad, 5, (float)deg_to_rad(72 * 4)
+    };
+    pen.set_color(Color::black);
+    pen.set_style(
+        Line_style(Line_style::Line_style_type::solid, 1));
+    win.attach(pen);
+
+
+    win.wait_for_button();
+
     return 0;
 }
