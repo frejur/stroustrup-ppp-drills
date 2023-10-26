e347af7bc5fb39e3d9468600be9e21a56a0a3a5b
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-10-26 13:08:10 +0200

    CH13 E9, start over

--- chapter_13/exercise/e09/hexagontiler.h
+++ chapter_13/exercise/e09/hexagontiler.h
@@ -23,20 +23,19 @@ class HexagonTiler : public Graph_lib::Shape
 public:
 	HexagonTiler(Graph_lib::Point center, int num_tiles, int radius);
 	Graph_lib::Point center() const { return c; };
-	Graph_lib::Point tile_center() const { return tc; };
-	int num_tiles() const { return tile_count; };
+	Graph_lib::Point tile_center(int t) const;
+	int num_tiles() const { return t_count; };
 	int radius() const { return r; };
-	int inradius() const { return ir; };
+	int inradius() const { return in_r; };
 private:
-	Graph_lib::Point c, tc; // center and last tile center
-	int r, ir; // radius and inradius
-	int tile_count;
-	int active_edge;
-	int active_tile;
+	Graph_lib::Point c;
+	int r, in_r; // radius and inradius
+	int t_count; // tile count
+	int act_s;   // index of active side
+	int act_t;   // index of active tile
 	int calculate_inradius() const;
-	void add_tile(const Graph_lib::Point& o, const int connecting_side);
+	void add_tile(const RegularHexagon& hex, const int connecting_side);
 	std::vector<std::unique_ptr<RegularHexagon>> tiles;
-	std::unique_ptr<TileOffset> offs;
 	void draw_lines() const;
 };
 
