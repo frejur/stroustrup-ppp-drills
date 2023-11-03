67dc30734759ef7a59f3ae2f76dedfd3bc447aaf
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-11-03 14:11:28 +0100

    CH02 E02 added

--- chapter_02/exercise/e01/e01.cpp
+++ chapter_02/exercise/e01/e01.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include "../../lib/std_lib_facilities.h"
 
 int main()
 {
-    std::cout "Hello, world!" << std::endl;
+    std::cout << "Hello, programming!" << '\n' <<
+    	"Here we go!" << std::endl;
+    keep_window_open();
+    return 0;
 }
