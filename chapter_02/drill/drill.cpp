6c0d049d4ee98f2a5c892f625a8fb23dc1e95b78
Fredrik Jureen <fredrik.jureen@gmail.com>
2023-11-03 14:05:48 +0100

    Chapter 2, drill

--- chapter_02/drill/drill.cpp
+++ chapter_02/drill/drill.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "../lib/std_lib_facilities.h"
 
 int main()
 {
-    std::cout "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    keep_window_open();
+    return 0;
 }
