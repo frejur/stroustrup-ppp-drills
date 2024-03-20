59d4b0540354514f828712abafa72ac969f005c0
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-03-20 15:28:06 +0100

    manual testing

--- chapter_11/exercise/e04/e04.cpp
+++ chapter_11/exercise/e04/e04.cpp
@@ -25,7 +25,7 @@ e04::Input_type e04::input_type(const std::string& s, int v)
 
 	ss.str("");
 	ss << std::hex << v;
-	if (ss.str() == s) {
+	if (ss.str() == help::tolower(s)) {
 		return Input_type::Hexadecimal;
 	}
 
@@ -38,21 +38,69 @@ e04::Input_type e04::input_type(const std::string& s, int v)
 	return Input_type::Unknown;
 }
 
+e04::Multi_stream& e04::Multi_stream::operator>>(Multi_input& mi)
+{
+	std::string in;
+	int v = 0;
+	if (src >> in) {
+		if (buf.eof()) {
+			buf.clear();
+		}
+		buf.str(in);
+		buf >> v;
+
+		if (buf.fail() || !buf.eof()) {
+			src.clear(std::ios_base::failbit);
+			return *this;
+		}
+
+		Input_type t{input_type(in, v)};
+		if (t == Input_type::Unknown) {
+			src.clear(std::ios_base::failbit);
+			return *this;
+		}
+
+		mi.type = t;
+		mi.value = v;
+		mi.input = in;
+	}
+	return *this;
+}
+
+std::ostream& e04::operator<<(std::ostream& os, e04::Input_type t)
+{
+	return os << input_type_name(t);
+}
+
+std::ostream& e04::operator<<(std::ostream& os, Multi_input mi)
+{
+	return os << '(' << mi.value << ", " << mi.type << ", " << mi.input << ')';
+}
+
 //------------------------------------------------------------------------------
 
 int main()
-try {
-	std::cout << "Ingest numbers written in dexicmal, hexadecimal or "
+{
+	std::cout << "Ingest numbers written in decimal, hexadecimal or "
 	             "octal syntax."
 	          << '\n'
 	          << "10 (dec) = 0xA (hex) = 012 (oct)" << '\n';
 
+	e04::Multi_stream ms{std::cin};
+	e04::Multi_input mi;
+	while (true)
+		try {
+			if (!(ms >> mi)) {
+				throw std::runtime_error("Bad input");
+			}
+			std::cout << mi << '\n';
+		} catch (const std::exception& e) {
+			std::cerr << "Error: " << e.what() << '\n';
+			ms.clear();
+		} catch (...) {
+			std::cerr << "Unknown error" << '\n';
+			ms.clear();
+		}
 	std::cout << "Press <ENTER> to exit" << '\n';
-	help::wait_for_enter();
-} catch (const std::exception& e) {
-	std::cerr << "Error: " << e.what() << '\n';
-	help::wait_for_enter();
-} catch (...) {
-	std::cerr << "Unknown error" << '\n';
 	help::wait_for_enter();
 }
--- chapter_11/exercise/e04/e04.h
+++ chapter_11/exercise/e04/e04.h
@@ -6,6 +6,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace e04 {
 
@@ -20,8 +21,17 @@ inline const std::string& file_path_input()
 //------------------------------------------------------------------------------
 
 enum class Input_type { Unknown = -1, Decimal, Hexadecimal, Octal };
+inline const std::string& input_type_name(Input_type t)
+{
+	static std::vector<std::string> names{"Unknown",
+	                                      "Decimal",
+	                                      "Hexadecimal",
+	                                      "Octal"};
+	return names.at(static_cast<int>(t) + 1);
+}
 
 Input_type input_type(const std::string& s, int v);
+std::ostream& operator<<(std::ostream& os, Input_type t);
 
 struct Multi_input
 {
@@ -30,15 +40,26 @@ struct Multi_input
 	std::string input;
 };
 
+std::ostream& operator<<(std::ostream& os, Multi_input mi);
+
 class Multi_stream
 {
 public:
 	Multi_stream(std::istream& is)
-	    : src(is){};
-	bool operator()
+	    : src(is)
+	{
+		buf.unsetf(std::ios_base::dec);
+	};
+	Multi_stream& operator>>(Multi_input& mi);
+	operator bool()
 	{
 		return !(src.fail() || src.bad()) && (buf.good() || buf.eof());
 	};
+	void clear()
+	{
+		src.clear();
+		buf.clear();
+	}
 
 private:
 	std::istream& src;
