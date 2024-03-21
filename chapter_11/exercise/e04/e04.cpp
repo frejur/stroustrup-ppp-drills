dcea5c2a1eae63f405f07e2f45d909baa9ff2c6f
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-03-21 10:09:38 +0100

    CH11E04 dec hex oct input

--- chapter_11/exercise/e04/e04.cpp
+++ chapter_11/exercise/e04/e04.cpp
@@ -1,5 +1,6 @@
 #include "e04.h"
 #include "../../lib/help.h"
+#include <iomanip>
 
 /* Chapter 11, Exercise 4: A program that reads integers entered in decimal,
  *                         hexadecimal and octal syntax - and prints
@@ -69,12 +70,105 @@ e04::Multi_stream& e04::Multi_stream::operator>>(Multi_input& mi)
 
 std::ostream& e04::operator<<(std::ostream& os, e04::Input_type t)
 {
-	return os << input_type_name(t);
+	return os << help::tolower(input_type_name(t, true));
 }
 
 std::ostream& e04::operator<<(std::ostream& os, Multi_input mi)
 {
-	return os << '(' << mi.value << ", " << mi.type << ", " << mi.input << ')';
+	return os << std::setw(8) << mi.input << ' ' << mi.type << ' '
+	          << " converts to " << std::setw(5) << mi.value << " decimal";
+}
+
+//------------------------------------------------------------------------------
+
+e04::Command e04::get_command(bool restore_buffer)
+{
+	std::string cmd_str;
+	std::getline(std::cin, cmd_str);
+	if (cmd_str.size() == 0) {
+		return {"", ""};
+	}
+
+	for (const Command& c : commands()) {
+		if (cmd_str == c.symbol) {
+			return c;
+		}
+	}
+	if (restore_buffer) {
+		std::cin.putback('\n');
+		for (int i = cmd_str.size() - 1; i >= 0; --i) {
+			std::cin.putback(cmd_str[i]);
+		}
+	}
+	return {"", ""};
+}
+
+void e04::consume_leftover_endl()
+{
+	char c = 0;
+	if (std::cin.get(c), c != '\n') {
+		std::cin.putback(c);
+	}
+}
+
+void e04::run_manual_input_mode()
+{
+	Multi_stream ms{std::cin};
+	Multi_input mi;
+
+	std::cout << '\n'
+	          << "Please enter some numbers in your desired syntax." << '\n'
+	          << "E.g. '10' (dec) = '0xA' (hex) = '012' (oct)" << '\n';
+	while (true)
+		try {
+			consume_leftover_endl();
+			if (get_command(true) == cmd_quit()) {
+				break;
+			}
+
+			if (!(ms >> mi)) {
+				throw std::runtime_error("Bad input");
+			}
+
+			std::cout << mi << '\n';
+		} catch (const std::exception& e) {
+			std::cerr << "Error: " << e.what() << '\n';
+			ms.clear();
+		} catch (...) {
+			std::cerr << "Unknown error" << '\n';
+			ms.clear();
+		}
+	std::cout << '\n' << "Press <ENTER> to return to main loop" << '\n';
+	help::wait_for_enter();
+}
+
+void e04::print_commands()
+{
+	std::cout << "Please enter one of the characters below to proceed:" << '\n';
+	for (const Command& c : commands()) {
+		std::cout << c.symbol << '\t' << c.name << '\n';
+	}
+}
+
+void e04::load_input_from_file(const std::string& file_path)
+{
+	std::ifstream ifs{file_path};
+	if (!ifs) {
+		throw std::runtime_error("Could not load '" + file_path + "'");
+	}
+	ifs.exceptions(ifs.exceptions() | std::ios_base::badbit);
+
+	Multi_stream ms{ifs};
+	Multi_input mi;
+
+	std::cout << '\n';
+
+	while (ms >> mi) {
+		std::cout << mi << '\n';
+	}
+
+	std::cout << '\n' << "Press <ENTER> to return to the main loop" << '\n';
+	help::wait_for_enter();
 }
 
 //------------------------------------------------------------------------------
@@ -84,23 +178,18 @@ int main()
 	std::cout << "Ingest numbers written in decimal, hexadecimal or "
 	             "octal syntax."
 	          << '\n'
-	          << "10 (dec) = 0xA (hex) = 012 (oct)" << '\n';
+	          << '\n';
 
-	e04::Multi_stream ms{std::cin};
-	e04::Multi_input mi;
-	while (true)
-		try {
-			if (!(ms >> mi)) {
-				throw std::runtime_error("Bad input");
-			}
-			std::cout << mi << '\n';
-		} catch (const std::exception& e) {
-			std::cerr << "Error: " << e.what() << '\n';
-			ms.clear();
-		} catch (...) {
-			std::cerr << "Unknown error" << '\n';
-			ms.clear();
+	e04::Command cmd;
+	while (cmd != e04::cmd_quit()) {
+		e04::print_commands();
+		cmd = e04::get_command();
+
+		if (cmd == e04::cmd_load_from_file()) {
+			e04::load_input_from_file(e04::file_path_input());
+		} else if (cmd == e04::cmd_manual_input()) {
+			e04::run_manual_input_mode();
 		}
-	std::cout << "Press <ENTER> to exit" << '\n';
-	help::wait_for_enter();
+	}
+	std::cout << "Bye!" << '\n';
 }
--- chapter_11/exercise/e04/e04.h
+++ chapter_11/exercise/e04/e04.h
@@ -18,15 +18,85 @@ inline const std::string& file_path_input()
 	return p;
 }
 
+struct Command
+{
+	std::string symbol;
+	std::string name;
+};
+
+inline bool operator==(const Command& cmd_a, const Command& cmd_b)
+{
+	return cmd_a.symbol == cmd_b.symbol;
+}
+inline bool operator!=(const Command& cmd_a, const Command& cmd_b)
+{
+	return !(cmd_a == cmd_b);
+}
+
+inline const Command& cmd_manual_input()
+{
+	static Command c{"a", "Manual input"};
+	return c;
+}
+
+inline const Command& cmd_load_from_file()
+{
+	static Command c{"b", "Load input from file"};
+	return c;
+}
+
+inline const Command& cmd_quit()
+{
+	static Command c{"q", "Exit"};
+	return c;
+}
+
+inline const std::vector<Command>& commands()
+{
+	static std::vector<Command> cmds{cmd_manual_input(),
+	                                 cmd_load_from_file(),
+	                                 cmd_quit()};
+	return cmds;
+}
+
 //------------------------------------------------------------------------------
 
+inline int names_max_size(const std::vector<std::string>& names)
+{
+	int max_sz = 0;
+	for (int i = 0; i < names.size(); ++i) {
+		max_sz = (max_sz < names[i].size() ? names[i].size() : max_sz);
+	}
+	return max_sz;
+}
+
+inline std::vector<std::string> make_eq_len(
+    const std::vector<std::string> strings, int max_len)
+{
+	std::vector<std::string> new_strings;
+	for (const std::string& s : strings) {
+		new_strings.push_back(s);
+		for (int i = max_len - s.size(); i > 0; --i) {
+			new_strings.back().push_back(' ');
+		}
+	}
+	return new_strings;
+}
+
 enum class Input_type { Unknown = -1, Decimal, Hexadecimal, Octal };
-inline const std::string& input_type_name(Input_type t)
+inline const std::string& input_type_name(Input_type t,
+                                          bool append_spaces = false)
 {
 	static std::vector<std::string> names{"Unknown",
 	                                      "Decimal",
 	                                      "Hexadecimal",
 	                                      "Octal"};
+	static int max_sz = names_max_size(names);
+	static std::vector<std::string> names_eq_len{make_eq_len(names, max_sz)};
+
+	if (append_spaces) {
+		return names_eq_len.at(static_cast<int>(t) + 1);
+	}
 	return names.at(static_cast<int>(t) + 1);
 }
 
@@ -65,6 +135,14 @@ private:
 	std::istream& src;
 	std::stringstream buf;
 };
+
+//------------------------------------------------------------------------------
+
+void consume_leftover_endl();
+Command get_command(bool restore_buffer = false);
+void run_manual_input_mode();
+void load_input_from_file(const std::string& file_path);
+void print_commands();
 
 //------------------------------------------------------------------------------
 } // namespace e04
