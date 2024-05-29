0bbb31be1362d3c93b37910b1e624dc0f6b7f2ee
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-05-29 18:21:29 +0200

    copy files, rename, edit CMakeList.txt

---
+++ chapter_13/exercise/e17/e15.cpp
@@ -0,0 +1,150 @@
+#include <exception>
+#include <iostream>
+#include <sstream>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <math.h>
+
+#include "../../lib/Debug_window.h"
+#include "../../lib/Graph.h"
+#include "../share/tiler/dyntile.h"
+#include "../share/help/inters.h"
+#include "triangletiler.h"
+
+// Exercise 15.
+// Use the RightTriangle class to tile the entire window with triangles.
+
+namespace GL = Graph_lib;
+
+//------------------------------------------------------------------------------
+
+const std::string& info_click()
+{
+	static const std::string s{
+	    "Click anywhere to place the initial triangle tile"};
+	return s;
+}
+
+const std::string& info_transform()
+{
+	static const std::string s{
+	    "Move the cursor to transform the triangle. Click to confirm"};
+	return s;
+}
+
+//------------------------------------------------------------------------------
+
+void e15()
+{
+	constexpr bool ENABLE_DEBUG{true};
+	constexpr bool ENABLE_CLICK{true};
+
+	constexpr int win_w{ 640 };
+	constexpr int win_h{ 480 };
+	GL::Point c{ static_cast<int>(win_w*0.5), static_cast<int>(win_h*0.5) };
+	Debug_window win{{10, 10},
+	                 win_w,
+	                 win_h,
+	                 "Tri Harder",
+	                 ENABLE_DEBUG,
+	                 ENABLE_CLICK};
+
+	fl_color(0);
+	const GL::Point o{200, 200};
+	const int t_w{300};
+	const int t_h{200};
+	Tile_lib::Triangle_tiler tiles{o, t_w, t_h, 64, 0};
+	win.attach(tiles);
+
+	dyntile::Dynamic_tile dyn_t{dyntile::Tile_type::Right_triangle, o, 64, 0};
+	win.attach(dyn_t);
+
+    dyntile::Window_and_tile pass_to_callback{win, dyn_t};
+
+	GL::Text info{{64, 32}, info_click()};
+	win.attach(info);
+
+	int count_logged = 0;
+	while (win.shown()) {
+		if (win.click_has_been_registered()) {
+			if (!dyn_t.is_transforming()) {
+				tiles.pause_drawing();
+				dyn_t.enable_transform();
+                hacky_redraw_tile(pass_to_callback); // transform by moving mouse
+				info.set_label(info_transform());
+				std::stringstream ss;
+				for (int i = 0; i < 4; ++i) {
+					Point corner = o;
+					if (i == 1) {
+						corner.x += t_w;
+					} else if (i == 2) {
+						corner.x += t_w;
+						corner.y += t_h;
+					} else if (i == 3) {
+						corner.y += t_h;
+					}
+					inters::Bary_coords b{inters::bary(corner,
+					                                   tiles.point(0),
+					                                   tiles.point(1),
+					                                   tiles.point(2))};
+				}
+			} else {
+				tiles.resume_drawing();
+				dyn_t.apply_transform();
+				dyn_t.disable_transform();
+				tiles.update_transform(dyn_t.origin(),
+				                       dyn_t.side_length(),
+				                       dyn_t.angle());
+				// win.log("Top left tile: "
+				//         + std::to_string(tiles.top_left_tile().x) + ", "
+				//         + std::to_string(tiles.top_left_tile().y) + "\n");
+
+				if (count_logged > 4) {
+					win.clear_logs();
+					count_logged = 0;
+				}
+				win.log("Angle: " + std::to_string(tiles.angle()) + "\n");
+				int sub_quadrant = static_cast<int>(tiles.angle()
+				                                    / (M_PI * 0.25));
+				win.log("Sub-Quadrant: " + std::to_string(sub_quadrant) + "\n");
+				++count_logged;
+
+				// win.log("Rotated Bbox Dim. "
+				//         + std::to_string(tiles.debug_bbox_width()) + " x "
+				//         + std::to_string(tiles.debug_bbox_height()) + "\n");
+
+				win.log(
+				    "Tiles bbox bounds: "
+				    + std::to_string(tiles.debug_rotates_bounds().min.x) + ", "
+				    + std::to_string(tiles.debug_rotates_bounds().min.y) + " | "
+				    + std::to_string(tiles.debug_rotates_bounds().max.x) + ", "
+				    + std::to_string(tiles.debug_rotates_bounds().max.y) + "\n");
+				std::vector<Graph_lib::Point> dp{
+				    tiles.debug_draw_tiles_bbox_grid()};
+				win.log("Origin: " + std::to_string(dp[0].x) + ", "
+				        + std::to_string(dp[0].y) + "\n");
+
+				info.set_label(info_click());
+			}
+		}
+		win.wait_for_click();
+	}
+}
+
+int main() {
+	try {
+          e15();
+          return 0;
+	}
+	catch (std::exception &e) {
+		std::cerr<<e.what()<<'\n';
+		return 1;
+	}
+	catch (...) {
+		std::cerr<<"Unknown error\n";
+		return 2;
+	}
+}
+
+
---
+++ chapter_13/exercise/e17/triangletiler.h
@@ -0,0 +1,38 @@
+#ifndef TRIANGLETILER_H
+#define TRIANGLETILER_H
+
+#include "../share/tiler/tiler.h"
+
+namespace Tile_lib { //--------------------------------------------------------
+
+class Triangle_tiler : public Tiler
+{
+public:
+	Triangle_tiler(
+	    Graph_lib::Point o, int w, int h, int tri_side, double rotation);
+
+private:
+	void add_tiles(const Graph_lib::Point pos,
+	               const int side_len,
+	               const float angle,
+	               const Tile_count count_a,
+	               const Tile_count count_b,
+	               const Graph_lib::Point offset_a,
+	               const Graph_lib::Point offset_b);
+	void add_tile(Graph_lib::Point pos, int side_len, float angle);
+	bool tile_is_inside(int idx);
+};
+
+//------------------------------------------------------------------------------
+
+inline Graph_lib::Point triangle_end_point(Graph_lib::Point pt,
+                                           float angle,
+                                           int side_len)
+{
+	return {static_cast<int>(std::round(pt.x + cos(angle) * side_len)),
+	        static_cast<int>(std::round(pt.y + sin(angle) * side_len))};
+}
+
+} // namespace Tile_lib
+
+#endif // TRIANGLETILER_H
