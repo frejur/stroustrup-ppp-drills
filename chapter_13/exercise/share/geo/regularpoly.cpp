c1f37382a059d98da79f9dfa932bfccde70209d1
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-05-30 08:42:06 +0200

    Add copies of Regular_polygon and Regular_hexagon code files to shared folder

---
+++ chapter_13/exercise/share/geo/regularpoly.cpp
@@ -0,0 +1,47 @@
+#define _USE_MATH_DEFINES
+#include "regularpoly.h"
+#include <cmath>
+
+RegularPolygon::RegularPolygon(
+    Graph_lib::Point o, int rr, int n, float a
+) :
+    origin ( o ), r ( rr ), num_sides ( n ),
+    angle { a }
+{
+    // guard against incompatible numbers
+    r = min(1280, max(10, r));
+    num_sides = min(16, max(3, num_sides));
+
+    add_poly_points();
+}
+
+void RegularPolygon::add_poly_points()
+{
+    double ang_incr { M_PI * 2.0 / num_sides };
+    int this_x { 0 }, this_y { 0 },
+        offset_x{ 0 }, offset_y{ 0 };
+
+    for (unsigned int i = 0; i < num_sides ; ++i) {
+        this_x = {
+            (int)round(
+                origin.x +
+                sin(M_PI - angle + ang_incr * i) * r
+            )
+        };
+        this_y = {
+            (int)round(
+                origin.y +
+                cos(M_PI - angle + ang_incr * i) * r
+            )
+        };
+        if (i == 0) {
+            add(origin);
+            offset_x = this_x - origin.x;
+            offset_y = this_y - origin.y;
+            c = { origin.x - offset_x, origin.y - offset_y };
+            continue;
+        }
+        add({ this_x - offset_x, this_y - offset_y });
+    }
+}
+
---
+++ chapter_13/exercise/share/geo/regularpoly.h
@@ -0,0 +1,23 @@
+#ifndef REGULAR_POLY_H
+#define REGULAR_POLY_H
+
+#include "../../lib/Graph.h"
+
+class RegularPolygon : public Graph_lib::Closed_polyline
+{
+public:
+    RegularPolygon(
+        Graph_lib::Point origin, int radius, int num_sides,
+        float angle = 0
+    );
+	int radius() const { return r; };
+	Graph_lib::Point center() const { return c; };
+private:
+    Graph_lib::Point origin;
+    Graph_lib::Point c;
+    int r, num_sides;
+    float angle;
+    void add_poly_points();
+};
+
+#endif // REGULAR_POLY_H
