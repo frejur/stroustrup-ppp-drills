9883eb35c03ed302c852badb5aa97735e0ef554c
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-08-28 02:11:21 +0200

    finish drill

--- chapter_14/drill/drill.cpp
+++ chapter_14/drill/drill.cpp
@@ -2,28 +2,110 @@
 #include <iostream>
 #include "drill.h"
 
+void drill_1_to_3()
+{
+	// Drill 1
+	d_1_3::B1 b1;
+	b1.f();
+	b1.vf();
+
+	// Drill 2
+	d_1_3::D1 d1;
+	d1.f();
+	d1.vf();
+
+	// Drill 3
+	d_1_3::B1& b1_ref{d1};
+	b1_ref.f();
+	b1_ref.vf();
+}
+
+void drill_4()
+{
+	// Drill 1
+	d_4::B1 b1;
+	b1.f();
+	b1.vf();
+
+	// Drill 2
+	d_4::D1 d1;
+	d1.f(); /* Outputs "D1::f()". Because it's not an overriden virtual function,
+	           the 'f()' function defined in D1 hides the function defined in B1. */
+	d1.vf();
+
+	// Drill 3
+	d_4::B1& b1_ref{d1};
+	b1_ref.f(); // Outputs "B1::f()", though one might expect "D1::f()", see above.
+	b1_ref.vf();
+}
+
+void drill_5()
+{
+	/*
+	// Drill 1
+	d_5::B1 b1;
+	b1.f();
+	b1.vf();
+
+	// Drill 2
+	d_5::D1 d1;
+	d1.f();
+	d1.vf();
+
+	// Drill 3
+	d_5::B1& b1_ref{d1};
+	b1_ref.f();
+	b1_ref.vf();
+	*/
+
+	std::cout << "Does not compile: cannot instantiate abstract class" << '\n';
+}
+
+void drill_6()
+{
+	d_6::D2 d2;
+	d2.f();
+	d2.vf();
+	d2.pvf();
+}
+
+void drill_7()
+{
+	d_7::D21 d21;
+	d_7::D22 d22;
+	d_7::f(d21);
+	d_7::f(d22);
+}
+
 int main()
 {
-    try
-    {
-        B1 b1;
-        b1.f();
-        std::cout << '\n';
-        b1.vf();
-        std::cout << '\n';
+	try {
+		std::cout << "Drill 1 to 3:" << '\n';
+		drill_1_to_3();
+		std::cout << '\n';
 
-        D1 d1;
-        d1.f();
-        std::cout << '\n';
-        d1.vf();
-        std::cout << '\n';
+		std::cout << "Drill 4:" << '\n';
+		drill_4();
+		std::cout << '\n';
 
-        return 0;
-    }
-    catch (std::exception& e) {
-        return 1;
-    }
-    catch (...) {
-        return 2;
-    }
+		std::cout << "Drill 5:" << '\n';
+		drill_5();
+		std::cout << '\n';
+
+		std::cout << "Drill 6:" << '\n';
+		drill_6();
+		std::cout << '\n';
+
+		std::cout << "Drill 7:" << '\n';
+		drill_7();
+
+		std::cout << '\n' << "Press <ENTER> to exit" << '\n';
+
+		std::cin.get();
+		return 0;
+	} catch (std::exception& e) {
+		return 1;
+	} catch (...) {
+		return 2;
+	}
 }
--- chapter_14/drill/drill.h
+++ chapter_14/drill/drill.h
@@ -1,24 +1,127 @@
 #ifndef DRILL_H
 #define DRILL_H
 #include <iostream>
+#include <sstream>
 
+inline void print_call(const std::string& cl_name, const std::string& fn_name)
+{
+	std::cout << cl_name << "::" << fn_name << '\n';
+}
+
+inline void print_call(const std::string& cl_prefix,
+                       int cl_num,
+                       const std::string& fn_name)
+{
+	std::ostringstream ss;
+	ss << cl_prefix << cl_num;
+	print_call(ss.str(), "pvf()");
+}
+//------------------------------------------------------------------------------
+
+namespace d_1_3 {
 class B1 {
 public:
-    void f() {
-        std::cout << NAME << "::" << "f()";
-    }
-    virtual void vf() {
-        std::cout << NAME << "::" << "vf()";
-    }
-private:
-    const std::string NAME{ "B1" };
+	void f() const { print_call("B1", "f()"); };
+	virtual void vf() const { print_call("B1", "vf()"); };
 };
 
-class D1 : B1 {
+class D1 : public B1
+{
 public:
-    void vf() {
-        std::cout << "D1" << "::" << "f()";
-    }
+	void vf() const override { print_call("D1", "vf()"); };
 };
+} // namespace d_1_3
+//------------------------------------------------------------------------------
+
+namespace d_4 {
+class B1
+{
+public:
+	void f() const { print_call("B1", "f()"); };
+	virtual void vf() const { print_call("B1", "vf()"); };
+};
+
+class D1 : public B1
+{
+public:
+	void f() const { print_call("D1", "f()"); };
+	void vf() const override { print_call("D1", "vf()"); };
+};
+} // namespace d_4
+//------------------------------------------------------------------------------
+
+namespace d_5 {
+class B1
+{
+public:
+	void f() const { print_call("B1", "f()"); };
+	virtual void vf() const { print_call("B1", "vf()"); };
+	virtual void pvf() const = 0;
+};
+
+class D1 : public B1
+{
+public:
+	void f() { print_call("D1", "f()"); };
+	void vf() const override { print_call("D1", "vf()"); };
+};
+} // namespace d_5
+//------------------------------------------------------------------------------
+
+namespace d_6 {
+class B1
+{
+public:
+	void f() const { print_call("B1", "f()"); };
+	virtual void vf() const { print_call("B1", "vf()"); };
+	virtual void pvf() const = 0;
+};
+
+class D1 : public B1
+{
+public:
+	void f() { print_call("D1", "f()"); };
+	void vf() const override { print_call("D1", "vf()"); };
+};
+
+class D2 : public D1
+{
+public:
+	void pvf() const override { print_call("D2", "pvf()"); };
+};
+} // namespace d_6
+//------------------------------------------------------------------------------
+
+namespace d_7 {
+class B2
+{
+public:
+	virtual void pvf() const = 0;
+};
+
+class D21 : public B2
+{
+public:
+	void pvf() const override { print_call(s, "pvf()"); };
+
+private:
+	std::string s{"D21"};
+};
+
+class D22 : public B2
+{
+public:
+	void pvf() const override { print_call("D", i, "pvf()"); };
+
+private:
+	int i{22};
+};
+
+void f(B2& o)
+{
+	o.pvf();
+}
+
+} // namespace d_7
 
 #endif // DRILL_H
