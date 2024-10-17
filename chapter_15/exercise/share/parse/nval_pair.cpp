d921404c37c59d8028f026d93c5a41cfa44186c6
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-10-17 14:24:30 +0200

    Add Type: name-value-value triplet

--- chapter_15/exercise/share/parse/nval_pair.cpp
+++ chapter_15/exercise/share/parse/nval_pair.cpp
@@ -39,3 +39,43 @@ std::istream& operator>>(std::istream& is, Name_value_pair& p)
 	p = {name, val};
 	return is;
 }
+
+std::istream& operator>>(std::istream& is, Name_value_value_triplet& t)
+{
+	char l_par{}, r_par{}; // parentheses
+	char l_quo{}, r_quo{}; // quotes
+	char cm_0{}, cm_1{};
+	double val_a{}, val_b{};
+
+	is >> l_par >> l_quo;
+	if (l_par != '(' || l_quo != '"') {
+		is.clear(std::ios_base::badbit);
+		return is;
+	}
+
+	bool found_r_quo = false;
+	std::string name;
+	while (is) {
+		is.get(r_quo);
+		if (r_quo == '"') {
+			found_r_quo = true;
+			break;
+		}
+		name += r_quo;
+	}
+
+	if (!found_r_quo) {
+		is.clear(std::ios_base::badbit);
+		return is;
+	}
+
+	is >> cm_0 >> val_a >> cm_1 >> val_b >> r_par;
+
+	if (!is || cm_0 != ',' || cm_1 != ',' || r_par != ')') {
+		is.clear(std::ios_base::badbit);
+		return is;
+	}
+
+	t = {name, val_a, val_b};
+	return is;
+}
--- chapter_15/exercise/share/parse/nval_pair.h
+++ chapter_15/exercise/share/parse/nval_pair.h
@@ -9,6 +9,14 @@ struct Name_value_pair
 	double value;
 };
 
+struct Name_value_value_triplet
+{
+	std::string name;
+	double value_a;
+	double value_b;
+};
+
 std::istream& operator>>(std::istream& is, Name_value_pair& p);
+std::istream& operator>>(std::istream& is, Name_value_value_triplet& t);
 
 #endif // NVAL_PAIR_H
