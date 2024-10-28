df3a7bf3ac26b5d6384a6a42b56115a9d42ef760
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-10-28 13:48:58 +0100

    Add Animated_window

---
+++ chapter_16/exercise/share/win/anim_win.cpp
@@ -0,0 +1,108 @@
+#include "anim_win.h"
+
+namespace {
+constexpr int min_refresh_rate_ms{100};
+constexpr int max_refresh_rate_ms{10'000};
+constexpr int max_duration_sec{60 * 2};
+} // namespace
+
+const int anm_win::Animated_window::default_rate_ms = 500;
+
+//------------------------------------------------------------------------------
+
+anm_win::Animated_window::Animated_window(Graph_lib::Point top_left,
+                                          int width,
+                                          int height,
+                                          const string& title,
+                                          int refresh_rate_ms)
+    : Graph_lib::Window(top_left, width, height, title)
+    , is_poll(false)
+    , rate_ms(valid_rate(refresh_rate_ms))
+    , passed_ms(0)
+    , dur_sec(max_duration_sec)
+{}
+
+//------------------------------------------------------------------------------
+
+static double wait_for_x_sec(double x_sec, double wait_interval_sec)
+{
+	std::time_t start_t = std::clock();
+	int num_clocks = static_cast<int>(x_sec * CLOCKS_PER_SEC);
+	int delta = 0;
+	while (delta < num_clocks) {
+		Fl::wait(wait_interval_sec);
+		delta = static_cast<int>(std::clock() - start_t);
+	}
+	return static_cast<double>(delta) / CLOCKS_PER_SEC;
+}
+
+void anm_win::Animated_window::start_polling(int duration_sec)
+{
+	// Manage state
+	if (is_poll) {
+		throw std::runtime_error("Polling already active");
+	}
+	dur_sec = valid_duration(duration_sec, rate_ms);
+	const int dur_ms = dur_sec * 1'000.0;
+	passed_ms = 0;
+
+	// Rate and wait interval in seconds
+	const double rs = rate_ms / 1'000.0;
+	const double wait_sec = rs * 0.1;
+
+	// Est. and track # of polling actions to perform
+	const int num_act = static_cast<int>(dur_sec / rs) + 1;
+	int act_count = 0;
+
+	// At minimum, perform action once
+	polling_action();
+	++act_count;
+
+	// Execute polling action for every x number of ms
+	while (shown() && act_count < num_act) {
+		// Wait
+		double t_diff = wait_for_x_sec(rs, wait_sec);
+		passed_ms = (std::min)(dur_ms,
+		                       passed_ms + static_cast<int>(t_diff * 1'000));
+
+		// Polling action, repeat if time passed indicates it
+		int expected_act_count = (passed_ms / rate_ms) + 1;
+		int do_act = static_cast<int>(expected_act_count - act_count);
+		for (int i = 0; i < do_act; ++i) {
+			polling_action();
+		}
+		act_count += do_act;
+		Fl::redraw();
+	}
+
+	// Reset state
+	is_poll = false;
+	dur_sec = 0;
+	passed_ms = 0;
+}
+
+//------------------------------------------------------------------------------
+
+int anm_win::Animated_window::valid_rate(int r_ms) const
+{
+	if (r_ms < min_refresh_rate_ms || r_ms > max_refresh_rate_ms) {
+		throw std::runtime_error(
+		    "Invalid refresh rate, expected a value between "
+		    + std::to_string(min_refresh_rate_ms) + " and "
+		    + std::to_string(max_refresh_rate_ms) + ", but got "
+		    + std::to_string(r_ms));
+	}
+	return r_ms;
+}
+
+int anm_win::Animated_window::valid_duration(int d_sec, int r_ms) const
+{
+	const int min_d_sec{static_cast<int>((r_ms / 1'000.0) + 0.5)};
+	if (d_sec < min_d_sec || d_sec > max_duration_sec) {
+		throw std::runtime_error("Invalid duration, expected a value between "
+		                         + std::to_string(min_d_sec) + " and "
+		                         + std::to_string(max_duration_sec)
+		                         + ", but got " + std::to_string(d_sec));
+	}
+	return d_sec;
+}
---
+++ chapter_16/exercise/share/win/anim_win.h
@@ -0,0 +1,41 @@
+#ifndef ANIMATED_WINDOW_H
+#define ANIMATED_WINDOW_H
+#include "../../../lib/Graph.h"
+#include "../../../lib/Window.h"
+
+namespace anm_win {
+
+class Animated_window : public Graph_lib::Window
+{
+public:
+	Animated_window(Graph_lib::Point top_left,
+	                int width,
+	                int height,
+	                const std::string& title,
+	                int refresh_rate_ms = default_rate_ms);
+	void start_polling(int duration_sec);
+
+	bool is_polling() const { return is_poll; };
+	int milliseconds_passed() const { return passed_ms; };
+	int polling_duration_in_seconds() const { return dur_sec; };
+
+protected:
+	static const int default_rate_ms;
+	virtual void polling_action() = 0;
+
+private:
+	bool is_poll;
+	int rate_ms;
+
+	int passed_ms;
+	int dur_sec;
+
+	void poll();
+
+	int valid_rate(int r_ms) const;
+	int valid_duration(int d_sec, int r_ms) const;
+};
+
+} // namespace anm_win
+
+#endif // ANIMATED_WINDOW_H
