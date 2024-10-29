740d3072e35d7395e4e1aa4ed35014a0ab5f33ec
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-10-29 10:03:55 +0100

    Add exercise description and notes in the comments

--- chapter_16/exercise/e05/e05.cpp
+++ chapter_16/exercise/e05/e05.cpp
@@ -2,6 +2,19 @@
 #include <iostream>
 
 // Chapter 16, exercise 5:
+//------------------------------------------------------------------------------
+// Design Notes:
+// 1. Expanded on the previous exercise by creating a Ouija board interface,
+//    the 'planchette' moves around to spell out characters read from text files.
+// 2. Each character (A-Z, 0-9) has a mapped coordinate, stored in a text file.
+// 3. Added a "Remap" mode, where users can assign new character positions
+//    by clicking on the board and typing the desired character.
+//        > This is (Hackily) managed by drawing a grid of <In_box> inputs.
+//        > This was used to create the default mappings.
+// 4. Implemented a state management system to switch between:
+//        > Normal, Remap, and Ouija modes.
+// 5. Added various helper functions and classes.
+//------------------------------------------------------------------------------
 
 int e05()
 {
