4dacccc44a60b7d9ed3ec615800ad8f6f455b672
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-11-12 12:04:22 +0100

    Update function signatures for CPP11: 'constexpr' to 'inline const'

--- chapter_16/exercise/e09/expr_parser/help/help.h
+++ chapter_16/exercise/e09/expr_parser/help/help.h
@@ -75,7 +75,7 @@ inline std::string copy_ch_from_end(std::string& s, int num_ch)
 	return extract_ch_from_end(s, num_ch, true);
 };
 
-inline constexpr int scale_factor(int decimal_count)
+inline const int scale_factor(int decimal_count)
 {
 	if (decimal_count < 1) {
 		return 1;
@@ -87,7 +87,7 @@ inline constexpr int scale_factor(int decimal_count)
 	return f;
 }
 
-inline constexpr int exp_f_from_int(int i)
+inline const int exp_f_from_int(int i)
 {
 	// Returns the factor of exponent, 1e0 = 1, 1e1 = 10, etc.
 	i = (i > 0) ? i : -i;
