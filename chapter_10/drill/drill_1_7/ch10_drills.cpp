6789fe59829af13d85228e3d636c34ce65676c72
Fredrik Jureen <fredrik.jureen@gmail.com>
2024-11-22 12:34:22 +0100

    [ch10] Reformulate description of drill, less verbose

--- chapter_10/drill/drill_1_7/ch10_drills.cpp
+++ chapter_10/drill/drill_1_7/ch10_drills.cpp
@@ -5,29 +5,21 @@
 #include <algorithm>
 
 /*
-1. Start a program to work with points, discussed in �10.4.
-   Begin by defining the data type Point that has two coordinate
-   members x and y.
+1. Define a Point type with x and y coordinates.
 
-2. Using the code and discussion in �10.4, prompt the user to
-   input seven (x,y) pairs. As the data is entered, store it in
-   a vector of Points called original_points.
+2. Prompt the user for seven (x, y) pairs and store them in a vector<Point>
+   called original_points.
 
-3. Print the data in original_points to see what it looks like.
+3. Print the contents of original_points.
 
-4. Open an ofstream and output each point to a file named
-   mydata.txt. On Windows, we suggest the .txt suffix to make it
-   easier to look at the data with an ordinary text editor
-   (such as WordPad).
+4. Output each point to mydata.txt using an ofstream.
 
-5. Close the ofstream and then open an ifstream for mydata.txt.
-   Read the data from mydata.txt and store it in a new vector
-   called processed_points.
+5. Close the ofstream, open an ifstream for mydata.txt, and read the data into
+   a new vector<Point> called processed_points.
 
-6. Print the data elements from both vectors.
+6. Print the contents of both vectors.
 
-7. Compare the two vectors and print Something's wrong! if the
-   number of elements or the values of elements differ.
+7. If the vectors differ in size or values, print "Something's wrong!".
 */
 
 constexpr int points_list_size{ 2 };
